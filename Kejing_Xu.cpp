f09168c740ce4f5a70588f8b119cf3943f158e27
Kejing-Xu <112922078+Kejing-Xu@users.noreply.github.com>
2022-11-14 19:28:15 -0600

    Create Kejing_Xu.cpp

---
+++ Kejing_Xu.cpp
@@ -0,0 +1,11 @@
+int main(){
+   string name="Kejing_Xu";
+   vector<string> interested;
+   interested.push_back("Data_Science");
+   interested.push_back("Software_Engineering");
+   interested.push_back("Product_Management");
+   string skills[]={"C++","Python","SQL","Technical_leadership","And_more"};
+   string phone="(217)607-9386";
+   string linkedin="https://www.linkedin.com/in/kejing-xu-6032b7217/";
+   return 0;
+ }
